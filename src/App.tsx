d6495523e521c66ca625631513a6e51dbe3159d4
nashe01 <mupindu021@gmail.com>
2025-09-29 01:23:29 +0200

    full demo awaits revies and update of designs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import MwanaWev from "./pages/MwanaWev";
 import PayWega from "./pages/PayWega";
@@ -19,7 +18,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <ScrollToTop />
         <Routes>
           <Route path="/" element={<MwanaWev />} />
           <Route path="/mwanawev" element={<MwanaWev />} />
