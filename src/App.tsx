cd9cd7374c2b75b92bb051bc2fd151ef7d49e8e1
nashe01 <mupindu021@gmail.com>
2025-09-29 01:44:46 +0200

    scroll up correction

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import MwanaWev from "./pages/MwanaWev";
 import PayWega from "./pages/PayWega";
@@ -12,12 +14,24 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+// Component to scroll to top on route changes
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<MwanaWev />} />
           <Route path="/mwanawev" element={<MwanaWev />} />
